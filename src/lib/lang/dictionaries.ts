cebf3ce80997251349d2050593c5519170d75cb6
LucasSantaCasa <Lucas_Sachs@outlook.com>
2026-02-20 12:26:41 +0000

    Feat > Add EN language

--- src/lib/lang/dictionaries.ts
+++ src/lib/lang/dictionaries.ts
@@ -1,7 +1,7 @@
 import 'server-only'
 
 const dictionaries = {
-  en: () => import('./dictionaries/pt.json').then(module => module.default),
+  en: () => import('./dictionaries/en.json').then(module => module.default),
   pt: () => import('./dictionaries/pt.json').then(module => module.default),
 }
 
